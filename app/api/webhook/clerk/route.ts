e323788a099a9ecd58d76d9b889ee671d170cd62
KusKuSwap <omerlev66@gmail.com>
2024-04-25 11:31:05 +0300

    fixed createUser type errors

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -59,13 +59,12 @@ export async function POST(req: Request) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: username!,
+      username: username,
       firstName: first_name,
       lastName: last_name,
       photo: image_url,
     };
 
-    // @ts-ignore
     const newUser = await createUser(user);    
 
     if (newUser) {
--- types/index.ts
+++ types/index.ts
@@ -1,9 +1,9 @@
 // ====== USER PARAMS
 export type CreateUserParams = {
   clerkId: string
-  firstName: string
-  lastName: string
-  username: string
+  firstName: string | null
+  lastName: string | null
+  username: string | null
   email: string
   photo: string
 }
